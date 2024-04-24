7d59a260ab1092176b479012767de401aad2014f
KevinLai02 <jihn.smith629@gmail.com>
2024-04-24 16:32:30 +0800

    add api

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+from ctransformers import AutoModelForCausalLM
+
+# Set gpu_layers to the number of layers to offload to GPU. Set to 0 if no GPU acceleration is available on your system.
+llm = AutoModelForCausalLM.from_pretrained(
+    "TheBloke/Chinese-Alpaca-2-13B-GGUF",
+      model_file="chinese-alpaca-2-13b.q5_K_M.gguf", 
+      model_type="alpaca"
+    )
+#print(llm("生成一段大約100字有關 [前端工程師，工作經歷3年] 的中文履歷自我介紹句子，不要用條列式"))
+app = Flask(__name__)
+@app.route("/resume")
+def resume():
+    return llm("生成一段大約100字有關 [前端工程師，工作經歷3年] 的中文履歷自我介紹句子，不要用條列式") 
