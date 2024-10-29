3c5956d04bd3ee559f280700a63e845a4822a783
KevinLai02 <jihn.smith629@gmail.com>
2024-10-29 14:04:21 +0800

    fix llama3 api can not be used issue

--- app.py
+++ app.py
@@ -95,7 +95,7 @@ def resumeLlama():
     introductionRes = g.chatmodel.invoke("用 [繁體中文] 生成一段大約150字有關 ["+ profession + "," + talent + "," + category + "] 的中文履歷自我介紹句子")
     workExperienceRes = g.chatmodel.invoke("用繁體中文將以下這段在其他" + category + "公司的工作經驗以條列式補足300字，不要加任何標題 [" + workExperience + "]")
     talentRes = g.chatmodel.invoke("使用者本身會[" + talent + "]這些專業技能，用繁體中文將他的專業技能敘述補足400字，不要引入任何標題 ")
-    return jsonify({"introduction": introductionRes, "workExperience": workExperienceRes, "talent": talentRes})
+    return jsonify({"introduction": introductionRes.content, "workExperience": workExperienceRes.content, "talent": talentRes.content})
 
     
 @app.route("/AIspeak/rateAnwser", methods=["POST"])
